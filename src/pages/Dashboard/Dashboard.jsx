92d643c3e887b9522d6f38b86f43855926543c8c
Ajarek <ajarek@poczta.onet.pl>
2023-03-23 23:33:54 +0100

    added dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 import './Dashboard.css'
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='dashboard'>
+      <div className="card">
+        <div className="question">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit?</p>
+         <span>1/10</span>
+        </div>
+        <button className="answer">React</button>
+        <button className="answer">React.js</button>
+        <button className="answer">ReactJS</button>
+        <button className="answer">All of the above</button>
+        <button className="answer next">Next Question</button>
+      </div>
+      
+      
+      
+    </div>
   )
 }
 
