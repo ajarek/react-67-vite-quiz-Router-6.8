d83533d0cd49ceb85d6631d7c7db9e53001c942d
Ajarek <ajarek@poczta.onet.pl>
2023-03-24 00:29:51 +0100

    added function nextQuestion

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Dashboard.css'
+import {Link,  useNavigate} from 'react-router-dom'
+import data from '../..//assets/data.json'
 const Dashboard = () => {
+  const [index,setIndex]=useState(0)
+  const currentData=data[index] 
+  const navigate = useNavigate();
+const nextQuestion=(e) => {
+  setIndex(index+1)
+  if(index+2===data.length){
+   e.target.style.background='#198754'
+   e.target.textContent='Show Result'
+  }
+  if(index+2>data.length){
+   navigate('/')
+  }
+}
+
   return (
     <div className='dashboard'>
       <div className="card">
         <div className="question">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit?</p>
-         <span>1/10</span>
+        <p>{currentData.question}</p>
+         <span>{index+1}/{data.length}</span>
         </div>
-        <button className="answer">React</button>
-        <button className="answer">React.js</button>
-        <button className="answer">ReactJS</button>
-        <button className="answer">All of the above</button>
-        <button className="answer next">Next Question</button>
+        <button className="answer">{currentData.options[0]}</button>
+        <button className="answer">{currentData.options[1]}</button>
+        <button className="answer">{currentData.options[2]}</button>
+        <button className="answer">{currentData.options[3]}</button>
+        <button className="answer next" onClick={nextQuestion}>Next Question</button>
       </div>
       
       
